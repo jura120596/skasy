c59be259c3f986a2f22dc154d818483dbb5be570
jura96_ <jura96_@mail.ru>
2022-08-09 15:45:46 +0300

    fix before release

--- resources/js/layout/layout-footer.vue
+++ resources/js/layout/layout-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer app class="app-footer">
-    <span style="height: 100%;width: calc( 100% - 45px); vertical-align: center;"
+    <span style="height: 100%;width: ( 100% - 30px); vertical-align: center; position: absolute"
           class="text-center d-flex justify-center align-center">{{$store.state.appName}}</span>
     <v-spacer/>
     <v-app-bar-nav-icon style="width: 45px" v-on:click="$emit('toggleDrawer')" />
